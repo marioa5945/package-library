36a37a191aa2711afaa34073b05644c0995d3d9e
marioa <marioa5945@gmail.com>
2021-02-25 23:49:18 +0800

    update build-com

--- scripts/node.build-com.ts
+++ scripts/node.build-com.ts
@@ -27,7 +27,7 @@ const buildCmdArr = [
 ];
 
 const publishCmdGet = (name: string) => {
-  return `cd ./dist/${name}/ && npm publish --access public && cd ../../`;
+  return `cd ./dist/${name}/ && npm publish --access public`;
 };
 
 let buildIndex = 0;
@@ -56,62 +56,36 @@ const selectBuildOrPublishPrint = () => {
 // print select of first
 selectBuildOrPublishPrint();
 
-let isPublish = true;
-
-/**
- * print of choose whether to publish
- */
-const selectIsPublishPrint = () => {
-  let str = '\r\nWhether to publish:\r\n';
-
-  str += `${isPublish ? '[X]' : '[ ]'} yes\r\n`;
-  str += `${!isPublish ? '[X]' : '[ ]'} no\r\n`;
-
-  process.stdout.write(str);
-};
-
 process.stdin.on('keypress', (s: unknown, key: any): void => {
   const name = key.name;
 
-  if (step === 1) {
-    if (name === 'up' || name === 'down') {
-      isPublish = !isPublish;
-    } else {
-      return;
-    }
+  if (name === 'up' && buildIndex > 0) {
+    buildIndex--;
+  } else if (name === 'down' && buildIndex < comCount - 1) {
+    buildIndex++;
+  } else if (name === 'down' && buildIndex === comCount - 1) {
+    buildIndex = 0;
+  } else if (name === 'up' && buildIndex === 0) {
+    buildIndex = comCount - 1;
   } else {
-    if (name === 'up' && buildIndex > 0) {
-      buildIndex--;
-    } else if (name === 'down' && buildIndex < comCount - 1) {
-      buildIndex++;
-    } else if (name === 'down' && buildIndex === comCount - 1) {
-      buildIndex = 0;
-    } else if (name === 'up' && buildIndex === 0) {
-      buildIndex = comCount - 1;
-    } else {
-      return;
-    }
+    return;
   }
 
   // Move the cursor to cover
-  readline.moveCursor(process.stdout, 0, step === 1 ? -4 : -comCount - 2);
+  readline.moveCursor(process.stdout, 0, -comCount - 2);
 
-  if (step === 0) {
-    selectBuildOrPublishPrint();
-  } else if (step === 1) {
-    selectIsPublishPrint();
-  }
+  selectBuildOrPublishPrint();
 });
 
 const execCmd = async (cmd: string) => {
   const { stdout, stderr } = await exec(cmd);
   console.log(stderr === '' ? stdout : stderr);
 
-  if (step === 3) {
+  if (step === 2) {
     console.log('\r\n===================== exit\r\n');
     process.exit(0);
   } else {
-    selectIsPublishPrint();
+    selectBuildOrPublishPrint();
   }
 };
 
@@ -121,9 +95,6 @@ rl.on('line', () => {
     execCmd(buildCmdArr[comCount - buildIndex - 1]);
     step++;
   } else if (step === 1) {
-    step = isPublish ? 2 : 1;
-    selectBuildOrPublishPrint();
-  } else if (step === 2) {
     console.log(`${choices[buildIndex]} is publish...`);
     step++;
     execCmd(publishCmdGet(choices[buildIndex]));
